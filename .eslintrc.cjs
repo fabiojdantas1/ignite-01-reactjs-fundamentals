d967a90b9c28ab09d254c40b65513c521f57d01b
Fábio Dantas <fabiojdantas1@gmail.com>
2022-08-01 22:47:05 -0300

    chore: add eslint and prettier to project

---
+++ .eslintrc.cjs
@@ -0,0 +1,46 @@
+module.exports = {
+    'env': {
+        'browser': true,
+        'es2021': true,
+        'node': true
+    },
+    'extends': [
+        'eslint:recommended',
+        'plugin:react/recommended'
+    ],
+    'parserOptions': {
+        'ecmaFeatures': {
+            'jsx': true
+        },
+        'ecmaVersion': 'latest',
+        'sourceType': 'module'
+    },
+    'plugins': [
+        'react'
+    ],
+    'rules': {
+        'indent': [
+            'error',
+            2
+        ],
+        'linebreak-style': [
+            'error',
+            'unix'
+        ],
+        'quotes': [
+            'error',
+            'single'
+        ],
+        'semi': [
+            'error',
+            'always'
+        ],
+        "react/react-in-jsx-scope": "off",
+        'react/prop-types': ['off']
+    },
+    'settings': {
+        'react': {
+          'version': "detect"
+        }
+      }
+};
--- src/App.jsx
+++ src/App.jsx
@@ -15,5 +15,5 @@ export function App() {
       />
 
     </div>
-  )
+  );
 }
