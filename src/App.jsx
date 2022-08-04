1fb78c9c48033f359dc1696f48361e00d1df4b05
Fábio Dantas <fabiojdantas1@gmail.com>
2022-08-04 19:42:53 -0300

    chore: add header component to project

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
+import { Header } from './components/Header';
 import { Post } from './Post';
+import './styles.css';
 
 export function App() {
 
   return (
     <div>
+      
+      <Header />
+      
       <Post 
         author='Fábio Dantas'
         content='Lorem ipsum dolor sit, amet consectetur adipisicing elit. A perferendis dolor facilis voluptates corporis ex sit dolorum aspernatur nisi, vel quo vero. Consectetur odio excepturi assumenda sunt, eum porro ullam!'
